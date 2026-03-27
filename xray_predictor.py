38b2c1772f5e27a20a6cd55763299c9f34064d2e
Rishik Jariwala <rishikjariwala54@gmail.com>
2026-03-27 23:28:54 +0530

    Value

--- xray_predictor.py
+++ xray_predictor.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
-from tkinter import Tk
+# from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 # Load model
